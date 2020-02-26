348a0f8194daebe2b7b7bd56249a0bdc89dd3728
Moshe <nicholas.namrot@gmail.com>
2020-02-26 15:55:19 -0500

    complex class added

--- Classes/ComplexNum.py
+++ Classes/ComplexNum.py
@@ -1,18 +1,24 @@
 import math
 
-
-class Complex(object):
+'''class Complex:
     def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
 
     def __add__(self, no):
+        return complex(self.real + no.real, self.imaginary + no.imaginary)
 
     def __sub__(self, no):
+        return complex(self.real - no.real, self.imaginary - no.imaginary)
 
     def __mul__(self, no):
+        return complex(self.real * no.real, self.imaginary * no.imaginary)
 
     def __truediv__(self, no):
+        return complex(self.real // no.real, self.imaginary // no.imaginary)
 
     def mod(self):
+        return complex(pow(self.real ** 2 + self.imaginary ** 2, 0.5), 0)
 
     def __str__(self):
         if self.imaginary == 0:
@@ -26,7 +32,27 @@ class Complex(object):
             result = "%.2f+%.2fi" % (self.real, self.imaginary)
         else:
             result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
-        return result
+        return result'''
+
+
+class Complex(complex):
+    def __add__(self, no):
+        return Complex(complex.__add__(self, no))
+
+    def __sub__(self, no):
+        return Complex(complex.__sub__(self, no))
+
+    def __mul__(self, no):
+        return Complex(complex.__mul__(self, no))
+
+    def __truediv__(self, no):
+        return Complex(complex.__truediv__(self, no))
+
+    def mod(self):
+        return Complex(complex.__abs__(self))
+
+    def __str__(self):
+        return '{0.real:.2f}{0.imag:+.2f}i'.format(self)
 
 
 if __name__ == '__main__':
